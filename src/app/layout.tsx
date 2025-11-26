3fda85456c96fb8f57e5e035b2ee4d2dd1f1683f
Legionite <komeyak@hawaii.edu>
2025-11-25 18:13:59 -1000

    Merged latest branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
