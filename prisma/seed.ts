5defa57b020b5a203470b2db17a06e3efeb49249
Justin Iwata <Justiniwata5@gmail.com>
2025-12-10 00:51:08 -1000

    testing

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,9 @@ import { PrismaClient, Role, Condition } from '@prisma/client';
 import { hash } from 'bcrypt';
 import * as config from '../config/settings.development.json';
 
+
 const prisma = new PrismaClient();
+
 
 async function main() {
   console.log('Seeding the database');
@@ -36,6 +38,62 @@ async function main() {
       },
     });
   }
+
+
+  // Seed schools
+  console.log('  Creating schools...');
+  await prisma.school.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      name: 'University of Texas at Austin',
+      location: 'Austin, TX',
+    },
+  });
+  await prisma.school.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      name: 'Massachusetts Institute of Technology',
+      location: 'Cambridge, MA',
+    },
+  });
+
+
+  // Seed tools
+  console.log('  Creating tools...');
+  await prisma.tool.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      name: 'Canvas',
+      category: 'Learning Management System',
+    },
+  });
+  await prisma.tool.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      name: 'Brightspace',
+      category: 'Learning Management System',
+    },
+  });
+  await prisma.tool.upsert({
+    where: { id: 3 },
+    update: {},
+    create: {
+      id: 3,
+      name: 'Google Classroom',
+      category: 'Learning Management System',
+    },
+  });
+
+
+  console.log('  Seeding complete!');
 }
 main()
   .then(() => prisma.$disconnect())
